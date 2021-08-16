c5b88b8afcc23b9db76a92c891548edeabbc631a
Nikitha <nikithanalumachu@gmail.com>
2021-08-16 23:59:46 +0100

    my first commit- get data from Api using Axios

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import axios from "axios";
 import './App.css';
-
+import {useEffect,useState} from 'react';
 function App() {
+
+  const [meaning, setMeanings] = useState([]);
+
+  const distApi = async() => {
+    try{
+      const data =await axios.get(
+        "https://api.dictionaryapi.dev/api/v2/entries/en/fill");
+      
+      setMeanings(data.data);
+          }
+          catch(error){
+            console.log(error);
+          }
+
+  };
+  console.log(meaning);
+  useEffect(()=>{
+    distApi();
+  },[]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>saibaba</h1>
     </div>
   );
 }
